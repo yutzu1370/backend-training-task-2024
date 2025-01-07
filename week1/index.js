185f0c0c638ff2dd3b6a95a2bd562b3269c477cd
yutzu1370 <doim1370523@gmail.com>
2025-01-07 16:59:40 +0800

    update 1

--- week1/index.js
+++ week1/index.js
@@ -8,6 +8,9 @@
 
 // 1.1 範例
 const alexAge = 25;
+const alexMembershipID = 'GYM2024-12345';
+const isRunningOnTreadmill = true;
+console.log(alexAge, alexMembershipID, isRunningOnTreadmill);
 
 // ### 題目二：變數命名練習
 // - 瑜伽團課 - 300 元
